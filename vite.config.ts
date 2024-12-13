9899b4b95892656dcccf734890744201fca93e42
Molon <1228639281@qq.com>
2024-12-13 19:55:32 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+      vue(),
+    AutoImport({
+    resolvers: [ElementPlusResolver()]
+  }),
+    Components({
+      dts: false,
+      resolvers: [ElementPlusResolver()]
+    }),],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/styles/variable.scss";`
+      }
+    }
+  },
+})
